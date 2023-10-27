b01359d3ac88d1d5764a6d9ecef3f983153c7ff9
Peter Tran <petertranminh22@gmail.com>
2023-10-27 19:13:42 -0400

    added endpoint for getting all posts

--- main/index.js
+++ main/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const utils = require("../other/utils");
 const supa = require("../other/database.js");
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,6 +23,25 @@ app.get("/getUserInfo/:username", async function (req, res) {
     }
     res.send(data);
 });
+
+app.get("/getAllPosts", async function (req, res) {
+  const data = await supa.supaClient
+    .from("posts")
+    .select('*')
+  
+  if (data["data"][0] === null || data === null) {
+    res.send({
+      error: null,
+      data: [],
+      count: null,
+      status: 400,
+      statusText: "Error",
+    });
+    return;
+  }
+
+  res.send(data);
+})
 
 app.post("/signUpUserInfo", async function (req, res) {
     var username = req.body["username"];
